0b062bfe9a174120501743c86f9dd9aa3153fae7
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2022-12-12 13:45:48 +0100

    Add project files.

---
+++ Krystof_20221212/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Krystof_20221212
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Soldier Mira;
+        Soldier Pepa;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Mira = new Soldier();
+            Mira.Jmeno = "Player";
+            Mira.Prijmeni = "1";
+            Zobraz(Mira, txtBox1);
+
+            Pepa = new Soldier();
+            Pepa.Jmeno = "Player";
+            Pepa.Prijmeni = "2";
+            Zobraz(Pepa, txtBox2);
+        }
+        public void Zobraz(Soldier soldier, TextBox textBox)
+        {
+            textBox.Text = soldier.Jmeno + " " + soldier.Prijmeni + "\n";
+            textBox.Text += "HP" + soldier.HP.ToString() + "\n";
+            textBox.Text += "Shield" + soldier.Shield.ToString() + "\n";
+            textBox.Text += "Level" + soldier.Level.ToString() + "\n";
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Pepa.Attack();
+            Zobraz(Pepa, txtBox2);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Mira.Attack();
+            Zobraz(Mira, txtBox1);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Mira.Heal();
+            Zobraz(Mira, txtBox1);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Pepa.Heal();
+            Zobraz(Pepa, txtBox2);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Pepa.SuperAttack();
+            Zobraz(Pepa, txtBox2);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Mira.SuperAttack();
+            Zobraz(Mira, txtBox1);
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            Mira.SuperHeal();
+            Zobraz(Mira, txtBox1);
+        }
+    }
+}
+
+        
+    
+
---
+++ Krystof_20221212/Soldier.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krystof_20221212
+{
+    public class Soldier
+    {
+        private int level;
+
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public int HP { get; set; }
+        public int Level { get => level; }
+        public int Shield { get; set; }
+
+        public Soldier()
+        {
+            Jmeno = "";
+            Prijmeni = "";
+            level = 1;
+            HP = 100;
+            Shield = 100;
+        }
+        public void Attack()
+        {
+            Shield -= 20;
+            if (Shield <= 0)
+            {
+                Shield = 0;
+                HP -= 20;
+            }
+
+        }
+
+        public void Heal()
+        {
+            Shield += 10;
+            if (Shield > 200)
+            {
+                Shield = 200;
+            }
+        }
+        public void SuperAttack()
+        {
+            Shield -= 40;
+            if (Shield <= 0)
+            {
+                Shield = 0;
+                HP -= 40;
+            }
+        }
+        public void SuperHeal()
+        {
+            Shield += 30;
+            if (Shield > 200)
+            {
+                Shield = 200;
+            }
+        }
+    }
+}
+
